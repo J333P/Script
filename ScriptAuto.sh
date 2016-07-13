3943879363203e81d7b807d04217b39c98a3837c
L'HOSTIS Jean-Pierre <jnpr.lh@gmail.com>
2016-07-13 13:00:27 +0200

    add script

---
+++ ScriptAuto.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# Script d'installation automatique meteor + Askle application
+#
+echo ----------------------debut-----------------------
+sudo apt-get install build-essential debian-keyring autoconf automake libtool flex bison scons nodejs npm git –y
+sudo apt-get update -y && sudo apt-get upgrade –y
+sudo git config --global user.name "J333P"
+sudo git config --global user.email jnpr.lh@gmail.com
+echo ----------------------meteor-----------------------
+git clone --depth 1 –b release-1.2.1-universal --single-branch http://github.com/4commerce-technologies-AG/meteor.git /home/pi/meteor
+cd /home/pi/meteor
+meteor run
+echo ----------------------askleWA-----------------------
+git clone http://github.com/J333P/AskleWA /home/pi/AskleWA
+cd /home/pi/AskleWA
+npm install --save meteor-node-stubs react react-addons-pure-render-mixin
+meteor add aldeed:tabular && meteor add twbs:bootstrap && meteor add react-meteor-data && meteor add meteorhacks:npm
+echo ----------------------fini-----------------------
+exit0
